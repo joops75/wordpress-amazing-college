33eb7160c62abf5c0b3cc72125ce9f58c8bd9824
Simon Cooper <kidney9@hotmail.com>
2020-01-09 17:56:04 +0000

    menu item highlighting functionality added

--- wp-content/themes/fictional-university-theme/footer.php
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -13,6 +13,9 @@
                     <div class="site-footer__col-two">
                         <h3 class="headline headline--small">Explore</h3>
                         <nav class="nav-list">
+                            <!-- <?php
+                                wp_nav_menu( array( 'theme_location' => 'footerMenuLocationOne' ) );
+                            ?> -->
                             <ul>
                                 <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
                                 <li><a href="#">Programs</a></li>
@@ -25,6 +28,9 @@
                     <div class="site-footer__col-three">
                         <h3 class="headline headline--small">Learn</h3>
                         <nav class="nav-list">
+                            <!-- <?php
+                                wp_nav_menu( array( 'theme_location' => 'footerMenuLocationTwo' ) );
+                            ?> -->
                             <ul>
                                 <li><a href="#">Legal</a></li>
                                 <li><a href="<?php echo site_url('/privacy-policy'); ?>">Privacy</a></li>
--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -10,6 +10,10 @@ function university_files() {
 add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features() {
+    // use WP to control menus if desired
+    // register_nav_menu( 'headerMenuLocation', 'Header Menu Location' );
+    // register_nav_menu( 'footerMenuLocationOne', 'Footer Menu Location One' );
+    // register_nav_menu( 'footerMenuLocationTwo', 'Footer Menu Location Two' );
     add_theme_support('title-tag');
 }
 

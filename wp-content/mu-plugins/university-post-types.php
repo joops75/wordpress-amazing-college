528ca352ada7b39bf80e2ec70817464e7edce506
Simon Cooper <kidney9@hotmail.com>
2020-01-12 17:10:14 +0000

    custom post type 'event' added

---
+++ wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,20 @@
+<?php
+
+function university_post_types() {
+    register_post_type( 'event', array(
+        // re-save permalink options in admin area to 'activate' any new custom post type permalink settings and avoid 404 errors
+        'rewrite' => array( 'slug' => 'events' ),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'singular_name' => 'Event',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events'
+        ),
+        'menu_icon' => 'dashicons-calendar',
+    ) );
+}
+
+add_action( 'init', 'university_post_types' );

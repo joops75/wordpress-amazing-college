3d49cd81827b2b276e22262e1958dc625ad07098
Simon Cooper <kidney9@hotmail.com>
2020-01-14 12:36:14 +0000

    acf event_date added with templates added/modified

--- wp-content/themes/fictional-university-theme/archive-event.php
+++ wp-content/themes/fictional-university-theme/archive-event.php
@@ -12,21 +12,31 @@
 </div>
 
 <div class="container container--narrow page-section">
-    <?php while( have_posts() ) : the_post(); ?>
+    <?php
+        while( have_posts() ) : the_post();
+        // posts are ordered and filtered by university_adjust_queries function in functions.php
+        // this is so paginate_links will work as expected without any extra code
+        $eventDate = new DateTime( get_field( 'event_date' ) );
+    ?>
         <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month"><?php the_time( 'M' ); ?></span>
-                <span class="event-summary__day"><?php the_time( 'd' ); ?></span>  
+                <span class="event-summary__month"><?php echo $eventDate->format('M'); ?></span>
+                <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>  
             </a>
             <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php echo wp_trim_words( get_the_content(), 20 ); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more &raquo;</a></p>
+                <p><?php echo has_excerpt() ? get_the_excerpt() : wp_trim_words( get_the_content(), 20 ); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more &raquo;</a></p>
             </div>
         </div>
     <?php
         endwhile;
         echo paginate_links();
     ?>
+
+    <hr class="section-break">
+
+    <p>Looking for a recap of past events? <a href="<?php echo site_url( '/past-events' ); ?>">Check out our past events archive</a>.</p>
+
 </div>
 
 <?php get_footer();
--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -16,17 +16,30 @@
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
             <?php
+                $today = date('Ymd'); // ensure date format returned from backend for 'event_date' custom field is also 'Ymd'
                 $eventPosts = new WP_Query( array(
                     'post_type' => 'event',
-                    'posts_per_page' => 2
+                    'posts_per_page' => 2,
+                    'meta_key' => 'event_date',
+                    'orderby' => 'meta_value_num',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        array(
+                            'key' => 'event_date',
+                            'compare' => '>=',
+                            'value' => $today,
+                            'type' => 'numeric' // not strictly necessary here
+                        )
+                    )
                 ) );
 
                 while( $eventPosts->have_posts() ) : $eventPosts->the_post();
+                $eventDate = new DateTime( get_field( 'event_date' ) );
             ?>
                 <div class="event-summary">
                     <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                        <span class="event-summary__month"><?php the_time( 'M' ); ?></span>
-                        <span class="event-summary__day"><?php the_time( 'd' ); ?></span>  
+                        <span class="event-summary__month"><?php echo $eventDate->format('M'); ?></span>
+                        <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>  
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>

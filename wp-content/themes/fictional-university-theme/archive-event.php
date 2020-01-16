c16ac80ec0972c01249103f79cc02b0556e3ec8b
Simon Cooper <kidney9@hotmail.com>
2020-01-16 18:52:59 +0000

    event summary sections moved to template-part file

--- wp-content/themes/fictional-university-theme/archive-event.php
+++ wp-content/themes/fictional-university-theme/archive-event.php
@@ -11,19 +11,7 @@
         while( have_posts() ) : the_post();
         // posts are ordered and filtered by university_adjust_queries function in functions.php
         // this is so paginate_links will work as expected without any extra code
-        $eventDate = new DateTime( get_field( 'event_date' ) );
-    ?>
-        <div class="event-summary">
-            <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month"><?php echo $eventDate->format('M'); ?></span>
-                <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>  
-            </a>
-            <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php echo has_excerpt() ? get_the_excerpt() : wp_trim_words( get_the_content(), 20 ); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more &raquo;</a></p>
-            </div>
-        </div>
-    <?php
+        get_template_part( 'template-parts/content', 'event' );
         endwhile;
         echo paginate_links();
     ?>

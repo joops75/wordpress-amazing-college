e2f8bf025f5f8966291f0fe598a0597e506c0c0a
Simon Cooper <kidney9@hotmail.com>
2020-01-22 12:14:45 +0000

    event planner user role added

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -5,6 +5,8 @@ function university_post_types() {
 
     // Event Post Type
     register_post_type( 'event', array(
+        'capability_type' => 'event',
+        'map_meta_cap' => true,
         'rewrite' => array( 'slug' => 'events' ),
         'has_archive' => true,
         'public' => true,
